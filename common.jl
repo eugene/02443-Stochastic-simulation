9cf333fa0c9c445b6fd40c0c6f18abe1f0534c02
Laurens <laurens.devos@outlook.com>
2017-06-12 16:04:34 +0200

    Exercise 4 finished

--- common.jl
+++ common.jl
@@ -13,3 +13,21 @@ using UnicodePlots
 #     PyPlot.clf()
 #     PyPlot.plt[:hist](args...;  kwargs...)
 # end
+
+"""
+Given value samples `values` from a continuous distribution, compute the
+confidence interval with significance level α.
+
+The distribution is assumed Student's-t: the mean and the variance is estimated.
+"""
+function confidence_interval(values; α=0.05)
+    n = length(values)
+    df = n - 1
+    μ = mean(values)
+    S = var(values)
+
+    lo = quantile(TDist(df), α/2)
+    hi = quantile(TDist(df), 1-α/2)
+
+    (μ + lo*(sqrt(S/n)), μ, μ + hi*(sqrt(S/n)))
+end
